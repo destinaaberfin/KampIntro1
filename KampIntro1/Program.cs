f7be3e42c99f2267b2ed5d4d88d19f97f3f0a8b8
Destina Berfin Seher <destinaberfiin@gmail.com>
2021-01-17 00:03:02 +0300

    16.01.2021 ders sonu

---
+++ KampIntro1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety - tip güvenliği
+            //Do not repeat
+            //değer tutucu, alias 
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrencisayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+       
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine("kategoriEtiketi");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention - isimlendirme kuralı
+        //syntax - yazım şekilleri
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi");
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler, Sepete Eklendi : " + urunAdi );
+
+
+        }
+    }
+}
